ac71b77b2587b910e4f7eeffce62f3f6ce5501eb
LarsBergqvist <lars.g.bergqvist@gmail.com>
2016-03-07 20:46:13 +0100

    Controller clean-up

--- WebAPI/static/Controller.js
+++ WebAPI/static/Controller.js
@@ -1,8 +1,11 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', [])
+    .config(function($interpolateProvider) {
+        $interpolateProvider.startSymbol('[[').endSymbol(']]');
+    });
 
-addImageToRelayObjects = function(relayObjects) {
+var addImageToRelayObjects = function(relayObjects) {
 	for (var i=0; i < relayObjects.length; i++) {
-  		relay = relayObjects[i];
+  		var relay = relayObjects[i];
 		if (relay.state == 'on') {
 			relay.image = '/static/on_button.gif';
 		}
@@ -10,15 +13,14 @@ addImageToRelayObjects = function(relayObjects) {
 			relay.image = '/static/off_button.gif';		
 		}
   	}	
-
 	return relayObjects;
-}
+};
 
 myApp.controller('RelaysController', ['$scope', '$http', function($scope, $http) {
 
   $scope.header = 'Relay status';
   
-  getRelayInfo = function() {
+  var getRelayInfo = function() {
     var promise = $http.get("api/relays");
 
     promise.then(function(response) {
@@ -30,26 +32,24 @@ myApp.controller('RelaysController', ['$scope', '$http', function($scope, $http)
   
   $scope.toggleRelay = function(relay) {
     var newState = 'off';
-    if (relay.state == 'off')
-    {
+    if (relay.state == 'off') {
     	newState = 'on';
     }
+    
     $http.put("/WebRelay/api/relays/"+relay.id, { state : newState}).then(function(response) {
     	relay = response.data.relay;
-	if (relay.state == 'on') {
-		relay.image = '/static/on_button.gif';
-	}
-	else {
-		relay.image = '/static/off_button.gif';		
-	}
-	for (var i=0; i < $scope.relays.length; i++) {
-		if ($scope.relays[i].id == relay.id)
-		{
-			$scope.relays[i] = relay;
-			break;
-		}
-	}
-    	
+        if (relay.state == 'on') {
+            relay.image = '/static/on_button.gif';
+        }
+        else {
+            relay.image = '/static/off_button.gif';		
+        }
+        for (var i=0; i < $scope.relays.length; i++) {
+            if ($scope.relays[i].id == relay.id) {
+                $scope.relays[i] = relay;
+                break;
+            }
+        }    	
     }, function(error) {
     }
     );
